0ef02776ea15bc12c8529fe126c7f3102fb336c7
qqaazz0222 <qqaazz0222@naver.com>
2024-06-04 14:07:16 +0900

    Create download.sh

---
+++ download.sh
@@ -0,0 +1,2 @@
+# wget "&download=1" -O filename
+wget "https://dguackr-my.sharepoint.com/:u:/g/personal/qqaazz0222_dgu_ac_kr/EVpn-2InifRPnVAjXbdpoEMBkTPm6gK2YXl8O1wFfbjoiw?e=FaVN3j&download=1" -O ./zip
